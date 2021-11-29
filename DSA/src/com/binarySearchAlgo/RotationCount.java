4725f8e835b15128185f66a30f42d05a648f923f
Shivam Mishra <shivamshesham.mishra@gmail.com>
2021-11-29 14:13:05 +0530

    Added code for Rotation Count

--- DSA/src/com/binarySearchAlgo/RotationCount.java
+++ DSA/src/com/binarySearchAlgo/RotationCount.java
@@ -4,7 +4,71 @@ public class RotationCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int [] arr = {4,5,6,7,0,1,2};
+		System.out.println(countRotation(arr));
 
 	}
+
+	private static int countRotation(int[] arr) {
+		// TODO Auto-generated method stub
+		int pivot = findPivot(arr);
+		return pivot+1;
+	}
+	
+	
+	static int findPivotWithDuplicate(int[] arr) {
+		int start = 0;
+		int end = arr.length-1;
+		
+		while (start<=end) {
+			int mid = start + (end-start)/2;
+			if(mid < end && arr[mid]>arr[mid+1]) {
+				return mid;
+			}
+			if(mid>start && arr[mid]<arr[mid-1]) {
+				return mid-1;
+			}
+			if(arr[mid]==arr[start] && arr[mid]==arr[end]) {
+				
+				if(arr[start]>arr[start+1]) {
+					return start;
+				}
+				start++;
+				if(arr[end]<arr[end-1]) {
+					return end - 1;
+				}
+				end--;
+			}
+			else if(arr[start]<arr[mid] || (arr[start]==arr[mid]&&arr[mid]<arr[end])) {
+				start = mid+1;
+			}else {
+				end = mid-1;
+			}
+		}
+		return -1;
+	}
+	
+	
+	static int findPivot(int[] arr) {
+		int start = 0;
+		int end = arr.length-1;
+		
+		while (start<=end) {
+			int mid = start + (end-start)/2;
+			if(mid < end && arr[mid]>arr[mid+1]) {
+				return mid;
+			}
+			if(mid>start && arr[mid]<arr[mid-1]) {
+				return mid-1;
+			}
+			if(arr[mid]<=arr[start]) {
+				end= mid-1;
+			}else {
+				start = mid+1;
+			}
+		}
+		return -1;
+	}
+
 
 }
